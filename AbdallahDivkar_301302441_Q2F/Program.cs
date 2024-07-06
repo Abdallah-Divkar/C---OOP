db23bf7f6c84e5ad16d991da1b7faa950e794a7a
Abdallah Divkar <67052449+Abdallah-Divkar@users.noreply.github.com>
2024-07-06 19:25:57 -0400

    Add files via upload

---
+++ AbdallahDivkar_301302441_Q2F/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace AbdallahDivkar_301302441_Q2F
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double petrolPrice;
+            string petrolType;
+
+            Console.Write("Enter the price of petrol : $");
+            petrolPrice = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter type: ");
+            petrolType = Console.ReadLine();
+
+            Console.WriteLine($"The cost of 25 litres of {petrolType} grade petrol will be ${petrolPrice * 25:F2} ");
+            Console.ReadLine();
+        }
+    }
+}
+
+//12 Marks
