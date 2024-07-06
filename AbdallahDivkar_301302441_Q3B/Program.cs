abd780f5f7038ec790ea07fc8eea57cde3fdfe1c
Abdallah Divkar <67052449+Abdallah-Divkar@users.noreply.github.com>
2024-07-06 19:27:03 -0400

    Add files via upload

---
+++ AbdallahDivkar_301302441_Q3B/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace AbdallahDivkar_301302441_Q3B
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char letter1;
+
+            Console.Write("Enter a letter: ");
+            letter1 = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine($"The number {(decimal)letter1:F1} is equivalent to the letter {letter1}");
+
+            Console.ReadLine();
+        }
+    }
+}
+
+//6 marks
+
